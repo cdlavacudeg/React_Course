3f1298994c2545f5b7a42a5b932eb5e9e0eeebd5
crislav10 <cristianlav10@gmail.com>
2021-09-27 12:08:09 -0500

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -33,18 +33,26 @@ class Main extends Component {
         leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
       );
     }
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/home" component={HomePage}/>
-        <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes} />}/>
-        <Route exact path="/contactus" component={Contact}/>
-        <Redirect to="/home" />
-      </Switch>
-      <Footer />
-    </div>
-  );
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes} />}/>
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact}/>
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
 }
 }
 
